4f23b0d627e006cdd24c89b93fe3175d7290647b
Marc Qualie <marc@marcqualie.com>
2012-02-11 16:12:09 +0000

    Broken Animation Prototype

--- js/soundparser.js
+++ js/soundparser.js
@@ -2,45 +2,46 @@ var SoundParser = function (file, container) {
 
 	var self = this;
 	
-	// Build audio reference
-	self.file = file;
-	self.container = document.querySelector('#' + container);
-	self.audio = new Audio();
-	self.audio.src = self.file;
-	self.audio.controls = true;
-	self.audio.autoplay = false;
-	self.audio.loop = true;
-	self.container.appendChild(self.audio);
-	
-	self.init = function () {
-	
-		// Build audiocontext
-		self.context = new webkitAudioContext();
-		self.analyser = self.context.createAnalyser();
-		self.source = self.context.createMediaElementSource(self.audio);
-	  	self.source.connect(self.analyser);
-	  	self.analyser.connect(self.context.destination);
-  	
-  	}
-	
-	// Access sound data
-	self.getVolume = function () {
+	//
+	//	Access sound data
+	//
+	self.getAverageVolume = function () {
 		return 0;
 	}
-	self.getRawData = function () {
-		var d = [];
+	self.getFrequencyData = function () {
 		var freqByteData = new Uint8Array(self.analyser.frequencyBinCount);
 		self.analyser.getByteFrequencyData(freqByteData);
-		d = freqByteData;
-//		console.log(self.analyser);
-//		console.log(self.source);
-//		console.log(freqByteData);
-		var t = 0;
-		for (var i = 0; i < 1024; i++) {
-			t += freqByteData[i];
-		}
-//		console.log('t:', t);
 		return freqByteData;
+	}
+	self.getAverageFrequency = function (bits) {
+		if (!bits) bits = 8;
+		var d = self.getFrequencyData();
+		var n = 1024 / bits;
+		var t = 0;
+		for (var i = 0; i < 1024; i += bits) {
+			t += d[i];
+		}
+		return t;
+	}
+	
+	//
+	//	Animation Helper
+	//
+	self.animate = function (params) {
+		
+		var animation = self.getAnimation(params.name);
+		
+		if (animation) {
+			var animateObject = new SoundParserAnimateObject(params);
+			setInterval(animation, 1000 / animateObject.fps, animateObject);
+		}
+		
+	}
+	self.getAnimation = function (name) {
+		
+		console.error('Animation not found');
+		return false;
+		
 	}
 	
 	// Stop/Start
@@ -51,6 +52,47 @@ var SoundParser = function (file, container) {
 		self.audio.pause();
 	}
 	
+	// Build audio reference
+	self.file = file;
+	self.container = document.querySelector('#' + container);
+	self.audio = new Audio();
+	self.audio.src = self.file;
+	self.audio.controls = true;
+	self.audio.autoplay = false;
+	self.audio.loop = true;
+	self.container.appendChild(self.audio);
+	
+	// Initialize context
+	self.init = function () {
+		self.context = new webkitAudioContext();
+		self.analyser = self.context.createAnalyser();
+		self.source = self.context.createMediaElementSource(self.audio);
+	  	self.source.connect(self.analyser);
+	  	self.analyser.connect(self.context.destination);
+ 
+  	}
+	
 	return self;
 
+}
+
+
+//
+//	Custom Animations
+//
+SoundParserAnimateObject = function (a) {
+	
+	 var self = function () {}
+	 
+	 self.fps = a.fps ? a.fps : 30;
+	 self.name = a.name ? a.name : 'default';
+	 self.canvas = a.canvas;
+	 
+	 return self;
+	
+}
+SoundParser.prototype.animations.flyingBird = function (ani) {
+	
+	
+	
 }

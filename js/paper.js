30b8f04175850e8e798fe7dd65a640e89e15b4b0
byrichardpowell <richardo@gmail.com>
2012-02-11 13:24:07 +0000

    first effect

--- js/paper.js
+++ js/paper.js
@@ -1,17 +1,42 @@
-// Create a circle shaped path at the center of the view,
-// with a radius of 70:
-var path = new Path.Circle(view.center, 70);
 
-// Fill the path with red:
-path.fillColor = 'red';
+
+circles = function(count) {
+
+	// The circles being rendered
+	var circles = []
+	
+
+	// Draw each circle
+	for (var i = count; i>=0; i--) {
+
+		console.log(i);
+		
+		circles[i] = new Path.Circle( view.center, 10 * i );
+		circles[i].fillColor = new RgbColor(0.1 * i, 0.35, 0.7, 0.1 * i); 
+
+	}
+
+	return circles;
+
+}
+
+
+var discs = circles(10); 
+
+
 
 
 function onFrame(event) {
 
+
     // Each frame, change the fill color of the path slightly by
     // adding 1 to its hue:
-    path.fillColor.hue += 1;
-    
-    ;
+    $.each( discs, function(i, disc) {
+    	
+    	disc.fillColor.hue += 1;
+
+    })
 
 }
+
+

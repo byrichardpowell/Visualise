35c03c94b68b958cc582b6dd5ac35e5fff503769
Marc Qualie <marc@marcqualie.com>
2012-02-11 14:42:39 +0000

    Basic working example

--- js/soundparser.js
+++ js/soundparser.js
@@ -4,20 +4,24 @@ var SoundParser = function (file, container) {
 	
 	// Build audio reference
 	self.file = file;
-	self.container = document.body;
+	self.container = document.querySelector('#' + container);
 	self.audio = new Audio();
 	self.audio.src = self.file;
 	self.audio.controls = true;
-	self.audio.autoplay = true;
+	self.audio.autoplay = false;
 	self.audio.loop = true;
 	self.container.appendChild(self.audio);
 	
-	// Build audiocontext
-	self.context = new webkitAudioContext();
-	self.analyser = self.context.createAnalyser();
-	self.source = self.context.createMediaElementSource(self.audio);
-  	self.source.connect(self.analyser);
-  	self.analyser.connect(self.context.destination);
+	self.init = function () {
+	
+		// Build audiocontext
+		self.context = new webkitAudioContext();
+		self.analyser = self.context.createAnalyser();
+		self.source = self.context.createMediaElementSource(self.audio);
+	  	self.source.connect(self.analyser);
+	  	self.analyser.connect(self.context.destination);
+  	
+  	}
 	
 	// Access sound data
 	self.getVolume = function () {
@@ -28,14 +32,14 @@ var SoundParser = function (file, container) {
 		var freqByteData = new Uint8Array(self.analyser.frequencyBinCount);
 		self.analyser.getByteFrequencyData(freqByteData);
 		d = freqByteData;
-		console.log(self.analyser);
-		console.log(self.source);
-		console.log(freqByteData);
+//		console.log(self.analyser);
+//		console.log(self.source);
+//		console.log(freqByteData);
 		var t = 0;
 		for (var i = 0; i < 1024; i++) {
 			t += freqByteData[i];
 		}
-		console.log('t:', t);
+//		console.log('t:', t);
 		return freqByteData;
 	}
 	
